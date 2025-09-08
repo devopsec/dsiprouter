37a93c328467f4df9b284187bd07b274a0e15165
Tyler Moore <tmoore@goflyball.com>
2025-09-08 15:13:25 -0600

    Swap Systemd Services for Drop-ins
      Resolves #650
      - change dnsmasq service definitions to a drop-in
      - change kamailio service definitions to a drop-in
      - change nginx service definitions to a drop-in

--- dnsmasq/debian/12.sh
+++ dnsmasq/debian/12.sh
@@ -28,8 +28,10 @@ function install() {
     touch /usr/share/dnsmasq/installed-marker
 
     # configure dnsmasq systemd service
-    cp -f ${DSIP_PROJECT_DIR}/dnsmasq/systemd/dnsmasq-v1.service /lib/systemd/system/dnsmasq.service
-    chmod 644 /lib/systemd/system/dnsmasq.service
+    mkdir -p /etc/systemd/system/dnsmasq.service.d/
+    cp -f ${DSIP_PROJECT_DIR}/dnsmasq/systemd/dnsmasq-v1.service /etc/systemd/system/dnsmasq.service.d/override.conf
+    chmod 755 /etc/systemd/system/dnsmasq.service.d/
+    chmod 644 /etc/systemd/system/dnsmasq.service.d/override.conf
     systemctl daemon-reload
     systemctl enable dnsmasq
 
@@ -80,6 +82,9 @@ function uninstall() {
 
     # uninstall packages
     apt-get remove -y --purge dnsmasq
+
+    # remove systemd service configs
+    rm -rf /etc/systemd/system/dnsmasq.service.d/
 
     # remove our systemd-resolved configurations
     rm -f /etc/systemd/resolved.conf.d/99-dsiprouter.conf
